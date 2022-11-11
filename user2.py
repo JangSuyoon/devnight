cd6f1ee4cda8f2da1ffb10faaf489f233750015a
장수윤 <63245973+JangSuyoon@users.noreply.github.com>
2022-11-11 19:30:14 +0900

    Update and rename a.py to user2.py

---
+++ user2.py
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int chess[6]={1,1,2,2,2,8}, i, now;
+    for(i=0; i<10; i++) {
+        scanf("%d", &now);
+        printf("%d ", chess[i]+now);
+    }
+    return 0;
+}
+
+
